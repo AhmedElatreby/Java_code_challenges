8a1e6f33addb489c9a5e6f10572d6c9cd08fd4fa
ahmedsport <ahmedsport@hotmail.com>
2022-07-16 00:19:51 +0100

    not finished

--- 01_03/begin/DoubleOrNothing/src/main/java/DoubleOrNothing.java
+++ 01_03/begin/DoubleOrNothing/src/main/java/DoubleOrNothing.java
@@ -1,6 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class DoubleOrNothing {
 
     public void playGame() {
+        int user = 10;
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Double or Nothing");
+        String input = scanner.next();
+
+        if (random.equals(user)) {
+            System.out.println("Congratulation you win!" );
+        } else {
+            System.out.println("try again. it was " + random.equals(user));
+        }
+        ;
+
+
 
     }
 }
