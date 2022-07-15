9c0bfa10750b02320bd0eae4c7dda8eaf1458d55
ahmedsport <ahmedsport@hotmail.com>
2022-07-15 23:43:11 +0100

    password

--- 01_02/begin/PasswordComplexity/src/main/java/PasswordComplexity.java
+++ 01_02/begin/PasswordComplexity/src/main/java/PasswordComplexity.java
@@ -3,7 +3,27 @@ import java.util.Scanner;
 public class PasswordComplexity {
 
     public static boolean isPasswordComplex(String password) {
-        return true;
+        if (password.length() < 6) {
+            return false;
+        }
+        boolean hasLowerCaseLetter = false;
+        boolean hasUpperCaseLetter = false;
+        boolean hasNumber = false;
+        for (int i = 0; i < password.length() ||
+                (!hasLowerCaseLetter
+                && ! hasUpperCaseLetter
+                && !hasNumber); i++) {
+            char current = password.charAt(i);
+            if (Character.isDigit(current)) {
+                hasNumber = true;
+            } else if (Character.isUpperCase(current)) {
+                hasLowerCaseLetter = true;
+            } else if (Character.isLowerCase(current)) {
+                hasUpperCaseLetter = true;
+            }
+
+        }
+        return hasNumber && hasLowerCaseLetter && hasUpperCaseLetter;
     }
 
     public static void main(String[] args) {
