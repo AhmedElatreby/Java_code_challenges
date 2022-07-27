75b10a854b19572cdea01362fd238796947bde1f
ahmedsport <ahmedsport@hotmail.com>
2022-07-27 14:42:50 +0100

    double or nothing

--- 01_03/begin/DoubleOrNothing/src/main/java/DoubleOrNothing.java
+++ 01_03/begin/DoubleOrNothing/src/main/java/DoubleOrNothing.java
@@ -2,24 +2,68 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class DoubleOrNothing {
+    private static int startingPoints = 10;
+    private Scanner scanner = new Scanner(System.in);
+    private Random random = new Random();
+    private int current = startingPoints;
+    private boolean isStillPlaying = true;
 
     public void playGame() {
-        int user = 10;
+        printWelcome();
+        askUserToContinue();
 
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
+        while (this.isStillPlaying){
+            printTryToDouble();
 
-        System.out.println("Double or Nothing");
-        String input = scanner.next();
-
-        if (random.equals(user)) {
-            System.out.println("Congratulation you win!" );
-        } else {
-            System.out.println("try again. it was " + random.equals(user));
+           if (shouldDouble()){
+               doubleCurrent();
+               printCongrats();
+               askUserToContinue();
+           } else{
+               printLost();
+               return;
+           }
         }
-        ;
+        printWalkAwayMessage();
+    }
 
+    private boolean shouldDouble() {
+        return this.random.nextInt(2) == 0;
+    }
 
+    private void printWalkAwayMessage() {
+        System.out.println("Congrats! you finish with " +
+                this.current + " points");
+    }
 
+    private void printLost() {
+        System.out.println("You've lost. " +
+                "No points for you");
+    }
+
+    private void printCongrats() {
+        System.out.println("Congrats! You now have " +
+                this.current + " points \n");
+    }
+
+    private void doubleCurrent() {
+        this.current = this.current * 2;
+    }
+
+    private void printTryToDouble() {
+        System.out.println("Let's try to double it!");
+    }
+
+    private void askUserToContinue() {
+        System.out.println("Would you like to continue " +
+                "and try to double your winnings? " +
+                "Enter YES to continue");
+        this.isStillPlaying = this.scanner.nextLine().toUpperCase().equals("YES");
+    }
+
+    private void printWelcome() {
+        System.out.println("Welcome to Double or Nothing game! " +
+                "We'll start you off with " + startingPoints +
+                " points\n");
     }
 }
